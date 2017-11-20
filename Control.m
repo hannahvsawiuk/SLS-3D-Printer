9f0b7d33c82eb3afe5a83e43f5fcaee537cf8f59
hannahvsawiuk <hannahvsawiuk@gmail.com>
2017-11-20 13:08:33 -0800

    Project 2 files

---
+++ Control.m
@@ -0,0 +1,35 @@
+% This script sets the controller parameters for the SLS 3-D Printer
+
+% ================
+% CONTROLLER GAINS
+% ================
+
+% Enter optimized PID values here.
+% No more than 3 significant figures per gain value.
+
+%PID0 = [1 0 0];
+%PID1 = [1 0 0];
+
+% Enter feedback sensor values here.
+
+%FB0 = 1;
+%FB1 = 1;
+
+
+% =====================
+% Set-Point Time Vector
+% =====================
+
+% The Time Vector is stored in a variable called "Time".
+% It's initial value is equally spaced for all samples and is
+% set in TRAJECTORY.M
+%
+% Redefine this vector here to optimize the build time of the part.
+% You can define it analytically or type in the elements manually but
+% you must not change the length because it must contain one value for
+% each Xd/Yd position pair.
+% In the Matlab window, enter "length(Time)" to see how big it is.
+
+% The Time vector must range from 0 to TotalTime
+
+%Time       = 0:SampleTime:TotalTime;       % DO NOT CHANGE TotalTime
