5677ececaed9647b0d5976b6abff6ce70217896a
hannahvsawiuk <hannahvsawiuk@gmail.com>
2017-11-30 21:54:02 -0800

    9:53pm

--- Control.m
+++ Control.m
@@ -3,22 +3,45 @@
 %============================================%
 % 			Controller Gains                 %
 %============================================%
-
+GHPID0 = zpk(tf([1.282e08],[1 15279.1702128 748862.340426 0]));
+GHPID1 = zpk(tf([1.4146e10],[1 40450.60 2044240 0]));
 % PID Values
 % ----------------------
 % No more than 3 significant figures per gain value.
 
+% Q0 scale factors
+SFP0 = 5;
+SFI0 = 1;
+SFD0 = 1;
+
+% Q0 constants
 Ku0 = 7.17*10^(-2);
+Kp0 = SFP0*(1.95)*(Ku0);
+Ki0 = SFI0*(96.77)*(Ku0);
+Kd0 = SFD0*(Ku0);
+
+% Q1 scale factors
+SFP1 = 1;
+SFI1 = 1;
+SFD1 = 1;
+
+% Q1 constants
 Ku1 = 1.83*10^(-3);
-Kp0 = (1.95)*(Ku0);
-Kp1 = (49.17)*(Ku1);
-Ki0 = (96.77)*(Ku0);
-Ki1 = (0)*(Ku1);
-Kd0 = (Ku0);
-Kd1 = (Ku1);
+Kp1 = SFP1*(49.17)*(Ku1);
+Ki1 = SFI1*(0)*(Ku1);
+Kd1 = SFD1*(Ku1);
 
 PID0 = [Kp0 Ki0 Kd0];
 PID1 = [Kp1 Ki1 Kd1];
+
+INT = tf(1,[1 0]); % integrator: 1/s
+N = 100; % filter coefficient
+
+pidt0 = zpk(Kp0 + Ki0*INT + Kd0*(N/(1+N*INT)));
+tune0_ol = pidt0*GHPID0;
+tune0 = feedback(tune0_ol,1);
+step(tune0);
+disp(stepinfo(tune0));
 
 % Feedback Sensor Values
 % ----------------------
